1a3fc71bb9cca1c6bc1f1613a5d2ea1a3e805c25
Ravichandran Sundaramurthy <lookatravi@Ravichandrans-MacBook-Pro.local>
2025-04-28 22:40:39 +0530

    update image version

--- python-service/test_app.py
+++ python-service/test_app.py
@@ -3,18 +3,15 @@ from app import app  # Import the Flask app from your app.py
 
 @pytest.fixture
 def client():
-    # Create a test client for the Flask app
     with app.test_client() as client:
-        yield client  # This client will be used in the test functions
+        yield client
 
 def test_home(client):
-    # Test the /api/python route
     response = client.get('/api/python')
     assert response.status_code == 200
-    assert b"Hello from Python Service!" in response.data  # Check for the string in the response body
+    assert b"Hello from Python Service!" in response.data
 
 def test_health(client):
-    # Test the /api/python/health route
     response = client.get('/api/python/health')
     assert response.status_code == 200
-    assert b"Health: OK" in response.data  # Check for the health status in the response body
+    assert b"All Systems Operational" in response.data  # Fixed line
